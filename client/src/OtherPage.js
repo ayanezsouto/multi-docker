1a5d7e005a2c3571e286a1d4b9b2abd1f41a30df
Andres Yañez <andresys@ext.inditex.com>
2020-01-24 13:10:48 +0100

    Initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+	return (
+		<div>
+			La pagina especial para Franklyn!
+			<Link to="/">Volver</Link>
+		</div>
+	);
+};
